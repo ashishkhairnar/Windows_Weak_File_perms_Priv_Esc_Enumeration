e6a55d32c186ec80c04a9cc38343a8afd3386307
Ashish Khairnar <ashish.khairnar1@gmail.com>
2018-08-05 01:26:56 -0400

    Update perms.ps1

--- perms.ps1
+++ perms.ps1
@@ -2,26 +2,17 @@ Get-childitem C:\ -Include "*.exe" -Recurse -ErrorAction SilentlyContinue | #Mod
 
 ForEach-Object {
 
-		            if (Test-Path -Path $_ -IsValid){
-		
-		
-			            try{
-			
-				                get-acl -Path $_.FullName |
+ if (Test-Path -Path $_ -IsValid){
+ try{
+      get-acl -Path $_.FullName |
 				
-					            select-object @{Name="Program"; Expression={$_.pschildname}},@{Name="Path";Expression={$_.pspath}},@{Name="Access";Expression={$_.accesstostring}} |			
+      select-object @{Name="Program"; Expression={$_.pschildname}},@{Name="Path";Expression={$_.pspath}},@{Name="Access";Expression={$_.accesstostring}}|			
 			                
-                                Where-Object {$_.accesstostring -contains "*modify*" | ft}    
-                        }
-			
-			
-			
-			
-			            catch{}
-		
-		
-		}
+      Where-Object {$_.accesstostring -contains "*modify*" | ft}    
+     }
 
+ catch{}
+ }
 }|Export-Csv -Path 'C:\Users\User1\Desktop\perms.csv' -NoTypeInformation
 
 $csv= Import-Csv 'C:\Users\User1\Desktop\perms.csv'
@@ -29,6 +20,5 @@ $csv= Import-Csv 'C:\Users\User1\Desktop\perms.csv'
 
 $csv | foreach {
 
-                if($_ -ilike "*Everyone Allow  FullControl*"){Write-Output $_}  #Modify for limited user like "Authenticated User  FullControl" etc. 
-
+    if($_ -ilike "*Everyone Allow  FullControl*"){Write-Output $_}  #Modify for limited user like "Authenticated User  FullControl" etc. 
 } | Export-Csv -Path "C:\Users\User1\Desktop\perms.csv" -NoTypeInformation
